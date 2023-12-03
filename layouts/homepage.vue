b636af2209c0db3978ca4305d2042e829c901d1b
Fabricio Flores <fabrifloresg@gmail.com>
2023-12-02 22:20:47 -0500

    Fix styles con homepage

---
+++ layouts/homepage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <header class="app-header">
+      <div class="app-logo">
+        <img src="/images/logo.svg">
+      </div>
+      <div class="app-user-menu">
+        <img src="/images/icons/house.svg">
+        <div class="name">Host</div>
+        <img src="/images/user.jpg" class="avatar">
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
